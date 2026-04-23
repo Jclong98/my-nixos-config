305d54f4ac49c5639be5a71dbe66d1166cc9d192
Jacob Long <jclong98@gmail.com>
2026-04-23 12:53:10 -0700

    ollama vulkan

--- modules/llm.nix
+++ modules/llm.nix
@@ -2,19 +2,23 @@
 
 {
   # Local LLM related services
-  # services.ollama = {
-  #   enable = true;
-  #   openFirewall = true;
+  services.ollama = {
+    enable = true;
+    openFirewall = true;
 
-  #   loadModels = [
-  #     "gemma4:e2b"
-  #   ];
-  # };
+    loadModels = [
+      "gemma4:e2b"
+      "qwen3.6:35b-a3b-q4_K_M"
+      "qwen3.6:27b-coding-mxfp8"
+    ];
+
+    package = pkgs.ollama-vulkan;
+  };
 
   # open-webui (port 8080 by default)
-  # services.open-webui = {
-  #   enable = true;
-  #   openFirewall = true;
-  #   host = "0.0.0.0";
-  # };
+  services.open-webui = {
+    enable = true;
+    openFirewall = true;
+    host = "0.0.0.0";
+  };
 }
