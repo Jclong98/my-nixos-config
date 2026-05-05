41f8b387743b3a50984cd092d8941e78a7a96cd4
Jacob Long <jclong98@gmail.com>
2026-05-04 23:33:26 -0700

    simplify openrgb

--- modules/openrgb.nix
+++ modules/openrgb.nix
@@ -1,42 +1,11 @@
-{ pkgs, ... }:
+# https://github.com/NixOS/nixpkgs/blob/nixos-25.11/nixos/modules/services/hardware/openrgb.nix
 
-let
-  set-green-rgb = pkgs.writeScriptBin "set-green-rgb" ''
-    #!/bin/sh
-    # Wait a moment for devices to be initialized
-    sleep 2
-    NUM_DEVICES=$(${pkgs.openrgb}/bin/openrgb --noautoconnect --list-devices | grep -E '^[0-9]+: ' | wc -l)
+{ pkgs, lib, ... }:
 
-    if [ "$NUM_DEVICES" -eq 0 ]; then
-      echo "No RGB devices found."
-      exit 0
-    fi
-
-    for i in $(seq 0 $((NUM_DEVICES - 1))); do
-      ${pkgs.openrgb}/bin/openrgb --noautoconnect --device $i --mode static --color 00FF00
-    done
-  '';
-in
 {
-  # Hardware service
-  services.hardware.openrgb.enable = true;
-
-  # Install package
-  environment.systemPackages = [ pkgs.openrgb ];
-
-  # Required for hardware access as per docs
-  services.udev.packages = [ pkgs.openrgb ];
-  boot.kernelModules = [ "i2c-dev" ];
-  hardware.i2c.enable = true;
-
-  # Systemd service to set color on boot
-  systemd.services.set-green-rgb = {
-    description = "Set RGB devices to green";
-    after = [ "multi-user.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${set-green-rgb}/bin/set-green-rgb";
-      Type = "oneshot";
-    };
+  services.hardware.openrgb = { 
+    enable = true; 
+    package = pkgs.openrgb-with-all-plugins; 
+    motherboard = "amd"; 
   };
 }
