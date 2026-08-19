210d6d2c338459a896cef84d7bb169e590701fd5
Jacob Long <jclong98@gmail.com>
2026-08-19 12:36:46 -0700

    fix(home): pin home-manager to release-26.05 to match nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
